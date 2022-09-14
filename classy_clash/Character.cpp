e5cd6b19213684e54263edd878396e27b486f7c8
Ryan <55786579+rbeatt@users.noreply.github.com>
2022-09-14 22:14:02 +0100

    Added props

---
+++ classy_clash/Character.cpp
@@ -0,0 +1,70 @@
+#include "Character.h"
+#include "raymath.h"
+
+Character::Character(int winWidth, int winHeight)
+{
+    width = texture.width / maxFrames;
+    height = texture.height;
+
+    screenPos =
+        {
+            static_cast<float>(winWidth) / 2.0f - scale * (0.5f * width),
+            static_cast<float>(winHeight) / 2.0f - scale * (0.5f * height)
+        };
+}
+
+void Character::tick(float deltaTime)
+{
+    worldPosLastFrame = worldPos;
+    Vector2 direction{};
+    if (IsKeyDown(KEY_A))
+        direction.x -= 1.0;
+    if (IsKeyDown(KEY_D))
+        direction.x += 1.0;
+    if (IsKeyDown(KEY_W))
+        direction.y -= 1.0;
+    if (IsKeyDown(KEY_S))
+        direction.y += 1.0;
+    if (Vector2Length(direction) != 0.0)
+    {
+        // set worldPos = worldPos + direction
+        worldPos = Vector2Add(worldPos, Vector2Scale(Vector2Normalize(direction), speed));
+        direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
+        texture = run;
+    }
+    else
+    {
+        texture = idle;
+    }
+
+    // update animation frame
+    runningTime += deltaTime;
+
+    if (runningTime >= updateTime)
+    {
+        frame++;
+        runningTime = 0.0;
+        if (frame > maxFrames)
+            frame = 0;
+    }
+
+    // draw the character
+    Rectangle source{frame * width, 0.f, rightLeft * width, height};
+    Rectangle dest{screenPos.x, screenPos.y, scale * width, scale * height};
+    DrawTexturePro(texture, source, dest, Vector2{}, 0.f, WHITE);
+}
+
+void Character::undoMovement()
+{
+    worldPos = worldPosLastFrame;
+}
+
+Rectangle Character::getCollisionRec()
+{
+    return Rectangle {
+        screenPos.x,
+        screenPos.y,
+        width * scale,
+        height * scale
+    };
+}
--- classy_clash/main.cpp
+++ classy_clash/main.cpp
@@ -1,78 +1,7 @@
-#include "raylib.h";
-#include "raymath.h";
-
-class Character
-{
-public:
-    Vector2 getWorldPos() { return worldPos; }
-    void setScreenPos(int winWidth, int winHeight);
-    void tick(float deltaTime);
-
-private:
-    Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
-    Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
-    ;
-    Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
-    ;
-    Vector2 screenPos{};
-    Vector2 worldPos{};
-    // 1: facing right, -1: facing left
-    float rightLeft{1.f};
-    // animation variables
-    float runningTime{1.f};
-    int frame{};
-    const int maxFrames{6};
-    const float updateTime{1.f / 12.f};
-    const float speed{4.f};
-};
-
-void Character::setScreenPos(int winWidth, int winHeight)
-{
-    screenPos =
-        {
-            (float)winWidth / 2.0f - 4.0f * (0.5f * (float)texture.width / 6.0f),
-            (float)winHeight / 2.0f - 4.0f * (0.5f * (float)texture.height)};
-};
-
-void Character::tick(float deltaTime)
-{
-    Vector2 direction{};
-    if (IsKeyDown(KEY_A))
-        direction.x -= 1.0;
-    if (IsKeyDown(KEY_D))
-        direction.x += 1.0;
-    if (IsKeyDown(KEY_W))
-        direction.y -= 1.0;
-    if (IsKeyDown(KEY_S))
-        direction.y += 1.0;
-    if (Vector2Length(direction) != 0.0)
-    {
-        // set worldPos = worldPos + direction
-        worldPos = Vector2Add(worldPos, Vector2Scale(Vector2Normalize(direction), speed));
-        direction.x < 0.f ? rightLeft = -1.f : rightLeft = 1.f;
-        texture = run;
-    }
-    else
-    {
-        texture = idle;
-    }
-
-    // update animation frame
-    runningTime += deltaTime;
-
-    if (runningTime >= updateTime)
-    {
-        frame++;
-        runningTime = 0.0;
-        if (frame > maxFrames)
-            frame = 0;
-    }
-
-    // draw the character
-    Rectangle source{frame * (float)texture.width / 6.f, 0.f, rightLeft * (float)texture.width / 6.f, (float)texture.height};
-    Rectangle dest{screenPos.x, screenPos.y, 4.0f * (float)texture.width / 6.0f, 4.0f * texture.height};
-    DrawTexturePro(texture, source, dest, Vector2{}, 0.f, WHITE);
-}
+#include "raylib.h"
+#include "raymath.h"
+#include "Character.h"
+#include "Prop.h"
 
 int main()
 {
@@ -82,8 +11,13 @@ int main()
 
     Texture2D map = LoadTexture("nature_tileset/OpenWorldMap24x24.png");
     Vector2 mapPos{0.0, 0.0};
-    Character knight;
-    knight.setScreenPos(windowWidth, windowHeight);
+    const float mapScale{4.0f};
+
+    Character knight{windowWidth, windowHeight};
+
+    Prop props[2]{
+        Prop{Vector2{600.f, 300.f}, LoadTexture("nature_tileset/Rock.png")},
+        Prop{Vector2{400.f, 500.f}, LoadTexture("nature_tileset/Log.png")}};
 
     SetTargetFPS(60);
     while (!WindowShouldClose())
@@ -94,8 +28,34 @@ int main()
         mapPos = Vector2Scale(knight.getWorldPos(), -1);
 
         // draw the map
-        DrawTextureEx(map, mapPos, 0.0, 4.0, WHITE);
+        DrawTextureEx(map, mapPos, 0.0, mapScale, WHITE);
+
+
+        //draw the props
+        for (auto prop : props)
+        {
+            prop.Render(knight.getWorldPos());
+        }
+
         knight.tick(GetFrameTime());
+
+        // check map bounds
+        if (knight.getWorldPos().x < 0.f ||
+            knight.getWorldPos().y < 0.f ||
+            knight.getWorldPos().x + windowWidth > map.width * mapScale ||
+            knight.getWorldPos().y + windowHeight > map.height * mapScale)
+        {
+            knight.undoMovement();
+        }
+
+        //check prop collisions
+        for (auto prop:props)
+        {
+            if (CheckCollisionRecs(prop.getCollisionRec(knight.getWorldPos()), knight.getCollisionRec()))
+            {
+                knight.undoMovement();
+            }
+        }
 
         EndDrawing();
     }
