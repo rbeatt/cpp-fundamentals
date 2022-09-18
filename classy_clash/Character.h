45505f272576ed97636b9f6260087426d36f60fb
Ryan <55786579+rbeatt@users.noreply.github.com>
2022-09-19 00:12:32 +0100

    Completed Classy Clash

--- classy_clash/Character.h
+++ classy_clash/Character.h
@@ -1,32 +1,25 @@
-#include "raylib.h"
+#ifndef CHARACTER_H
+#define CHARACTER_H
 
-class Character
+#include "raylib.h"
+#include "BaseCharacter.h"
+
+class Character : public BaseCharacter
 {
 public:
     Character(int winWidth, int winHeight);
-    Vector2 getWorldPos() { return worldPos; }
-    void tick(float deltaTime);
-    void undoMovement();
-    Rectangle getCollisionRec();
+    virtual void tick(float deltaTime) override;
+    virtual Vector2 getScreenPos() override;
+    Rectangle getWeaponCollisionRec() { return weaponCollisionRec; }
+    float getHealth() const { return health; }
+    void takeDamage(float damage);
 
 private:
-    Texture2D texture{LoadTexture("characters/knight_idle_spritesheet.png")};
-    Texture2D idle{LoadTexture("characters/knight_idle_spritesheet.png")};
-    ;
-    Texture2D run{LoadTexture("characters/knight_run_spritesheet.png")};
-    ;
-    Vector2 screenPos{};
-    Vector2 worldPos{};
-    Vector2 worldPosLastFrame{};
-    // 1: facing right, -1: facing left
-    float rightLeft{1.f};
-    // animation variables
-    float runningTime{1.f};
-    int frame{};
-    int maxFrames{6};
-    float updateTime{1.f / 12.f};
-    float speed{4.f};
-    float width{};
-    float height{};
-    float scale{4.0f};
+    int windowWidth{};
+    int windowHeight{};
+    Texture2D weapon{LoadTexture("characters/weapon_sword.png")};
+    Rectangle weaponCollisionRec{};
+    float health{100.f};
 };
+
+#endif
--- classy_clash/main.cpp
+++ classy_clash/main.cpp
@@ -2,6 +2,8 @@
 #include "raymath.h"
 #include "Character.h"
 #include "Prop.h"
+#include "Enemy.h"
+#include <string>
 
 int main()
 {
@@ -19,6 +21,25 @@ int main()
         Prop{Vector2{600.f, 300.f}, LoadTexture("nature_tileset/Rock.png")},
         Prop{Vector2{400.f, 500.f}, LoadTexture("nature_tileset/Log.png")}};
 
+    Enemy goblin{
+        Vector2{800.f, 300.f},
+        LoadTexture("characters/goblin_idle_spritesheet.png"),
+        LoadTexture("characters/goblin_run_spritesheet.png")};
+
+    Enemy slime{
+        Vector2{500.f, 700.f},
+        LoadTexture("characters/slime_idle_spritesheet.png"),
+        LoadTexture("characters/slime_run_spritesheet.png")};
+
+    Enemy *enemies[]{
+        &goblin,
+        &slime};
+
+    for (auto enemy : enemies)
+    {
+        enemy->setTarget(&knight);
+    }
+
     SetTargetFPS(60);
     while (!WindowShouldClose())
     {
@@ -30,11 +51,23 @@ int main()
         // draw the map
         DrawTextureEx(map, mapPos, 0.0, mapScale, WHITE);
 
-
-        //draw the props
+        // draw the props
         for (auto prop : props)
         {
             prop.Render(knight.getWorldPos());
+        }
+
+        if (!knight.getAlive()) // character not alive
+        {
+            DrawText("Game Over!", 55.f, 45.f, 40, RED);
+            EndDrawing();
+            continue;
+        }
+        else // character alive
+        {
+            std::string knightsHealth = "Health: ";
+            knightsHealth.append(std::to_string(knight.getHealth()), 0, 5);
+            DrawText(knightsHealth.c_str(), 55.f, 45.f, 40, RED);
         }
 
         knight.tick(GetFrameTime());
@@ -48,12 +81,28 @@ int main()
             knight.undoMovement();
         }
 
-        //check prop collisions
-        for (auto prop:props)
+        // check prop collisions
+        for (auto prop : props)
         {
             if (CheckCollisionRecs(prop.getCollisionRec(knight.getWorldPos()), knight.getCollisionRec()))
             {
                 knight.undoMovement();
+            }
+        }
+
+        for (auto enemy : enemies)
+        {
+            enemy->tick(GetFrameTime());
+        }
+
+        if (IsMouseButtonPressed(MOUSE_LEFT_BUTTON))
+        {
+            for (auto enemy : enemies)
+            {
+                if (CheckCollisionRecs(enemy->getCollisionRec(), knight.getWeaponCollisionRec()))
+                {
+                    enemy->setAlive(false);
+                }
             }
         }
 
